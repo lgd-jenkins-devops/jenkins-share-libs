409f1a397d3de6a49badbba0b4d176e5d68a9227
Luis <luis@10.10.5.100>
2024-11-08 12:24:07 -0500

    init

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -1,4 +1,4 @@
-def upload(String filePath, String bucketName) {
+def call(String filePath, String bucketName) {
     echo "Upload  ${filePath} to the bucket ${bucketName}..."
     sh """
         ./src/upload.sh ${filePath} ${bucketName}
