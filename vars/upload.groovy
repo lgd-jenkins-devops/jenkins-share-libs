76261c9e20a5a4851784a530f78dd231808d2e8d
Luis <luis@10.10.5.100>
2024-11-08 12:59:38 -0500

    set credentials

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -1,6 +1,7 @@
 def uploadToBucket(String filePath, String bucketName) {
     echo "Upload  ${filePath}"
     sh """
+        gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
         gsutil cp ${filePath} gs://${bucketName}/
     """
 }
